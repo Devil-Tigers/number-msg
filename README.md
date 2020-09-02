7d3828882a906bba08463afe1cec1ec751cb3cc7
tigerhackmalayalam <thmalayalamchannel@gmail.com>
2020-09-02 23:51:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+WELCOME TO "TIGER HACK MALAYALAM"
+
+This tool is used to Make UNLIMITED NUMBER Messages
+
+It will automatically make the numbers from 1 to the number you entered
+
+To use this tool
+
+commands 
+
+
+apt update
+
+
+apt upgrade
+
+
+pkg install git
+
+
+git clone 
+
+
+cd 
+
+
+bash 
+
+or 
+
+chmod +x * 
+
+
+./
+
+
+After 
+
+
+Enter a number 
+
+
+Then enter 
+
+
+After finishing 
+
+Type :- 
+
+
+
+ls
+
+cp num_msg /sdcard
+
+
+
+Now you can see a text file on your internal storage/file manager.
+
+
+Open that file 
+
+
+And copy. The all numbers
+
+
+And send.............
+
+
+ENJOY...
+
+FOR MORE DETAILS
+
+Subscribe my YouTube channel :- "Tiger Hack Malayalam"
+
+
+
+Link :-  https://www.youtube.com/c/thmalayalam
+
+
+
+*****FOR EDUCATIONAL PURPOSE ONLY*****
---
+++ number.sh
@@ -0,0 +1,92 @@
+﻿#!/bin/bash
+clear
+rm -rf num_msg.txt
+clear
+echo " "
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo " "
+echo "▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄      ▄▄▄▄   ▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄
+ ▀▀▀██▀▀▀   ▀▀██▀▀    ██▀▀▀▀█  ██▀▀▀▀▀▀  ██▀▀▀▀██
+    ██        ██     ██        ██        ██    ██
+    ██        ██     ██  ▄▄▄▄  ███████   ███████
+    ██        ██     ██  ▀▀██  ██        ██  ▀██▄
+    ██      ▄▄██▄▄    ██▄▄▄██  ██▄▄▄▄▄▄  ██    ██
+    ▀▀      ▀▀▀▀▀▀      ▀▀▀▀   ▀▀▀▀▀▀▀▀  ▀▀    ▀▀▀ "
+echo "▄▄    ▄▄     ▄▄        ▄▄▄▄   ▄▄   ▄▄▄
+ ██    ██    ████     ██▀▀▀▀█  ██  ██▀
+ ██    ██    ████    ██▀       ██▄██
+ ████████   ██  ██   ██        █████
+ ██    ██   ██████   ██▄       ██  ██▄
+ ██    ██  ▄██  ██▄   ██▄▄▄▄█  ██   ██▄
+ ▀▀    ▀▀  ▀▀    ▀▀     ▀▀▀▀   ▀▀    ▀▀ "
+echo " " 
+echo "===================================================="
+echo " "
+echo ".....Script by Tiger Hack Malayalam....."
+echo " "
+echo "---------------------------------------------"
+echo "By Clicking ENTER you Agree That.."
+echo "This tool is using only for "
+echo "FOR EDUCATIONAL PURPOSE ONLY"
+echo "---------------------------------------------"
+echo " "
+echo " ENTER HOW MANY NUMBERS YOU NEED : "
+echo ">>>"
+read count
+number=0
+echo "**********************************************"
+echo "    Please wait...."
+echo "    Making Numbers message file... "
+
+
+while :
+do
+number=$((number + 1))
+echo $number >> num_msg.txt
+if [[ $number == $count ]];
+then 
+break
+fi
+
+done
+clear
+echo " "
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo " "
+echo "▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄      ▄▄▄▄   ▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄
+ ▀▀▀██▀▀▀   ▀▀██▀▀    ██▀▀▀▀█  ██▀▀▀▀▀▀  ██▀▀▀▀██
+    ██        ██     ██        ██        ██    ██
+    ██        ██     ██  ▄▄▄▄  ███████   ███████
+    ██        ██     ██  ▀▀██  ██        ██  ▀██▄
+    ██      ▄▄██▄▄    ██▄▄▄██  ██▄▄▄▄▄▄  ██    ██
+    ▀▀      ▀▀▀▀▀▀      ▀▀▀▀   ▀▀▀▀▀▀▀▀  ▀▀    ▀▀▀ "
+echo "▄▄    ▄▄     ▄▄        ▄▄▄▄   ▄▄   ▄▄▄
+ ██    ██    ████     ██▀▀▀▀█  ██  ██▀
+ ██    ██    ████    ██▀       ██▄██
+ ████████   ██  ██   ██        █████
+ ██    ██   ██████   ██▄       ██  ██▄
+ ██    ██  ▄██  ██▄   ██▄▄▄▄█  ██   ██▄
+ ▀▀    ▀▀  ▀▀    ▀▀     ▀▀▀▀   ▀▀    ▀▀ "
+echo " " 
+echo "===================================================="
+echo " "
+echo ".....Script by Tiger Hack Malayalam....."
+echo " "
+echo "-------------------------------------------------"
+echo "   Your Numbers File Created SUCCESSFULLY.. "
+echo "-------------------------------------------------"
+echo " "
+echo "   Now Type:- cp num_msg.txt /sdcard "
+echo "   Now open your Internal storage  "
+echo "   There you can see "num_msg.txt" File "
+echo "   Open that File And Copy the Numbers "
+echo "   And SEND TO ANYONE........."
+echo " "
+echo "================================================="
+echo " "
+echo "   For More Details.."
+echo "   Subscribe My Channel :- TIGER HACK MALAYALAM"
+echo "   Link:- https://www.youtube.com/c/thmalayalam "
+echo "   Email:- thmalayalamchannel@gmail.com "
+echo " "
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
